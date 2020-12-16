0712acbc26cf821a8ffc5efc9cd4c5c3c5712666
will.wu <will.wu@eitc.com.tw>
2020-12-16 15:31:40 +0800

    Updates

---
+++ precache-manifest.7fd158966a8d5714f5b8495be74e68a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "706b5197601b620a4383",
+    "url": "./static/css/main.edb1d0b7.chunk.css"
+  },
+  {
+    "revision": "706b5197601b620a4383",
+    "url": "./static/js/main.706b5197.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "cecde8efa8f2bad3038e",
+    "url": "./static/css/2.e5ee0802.chunk.css"
+  },
+  {
+    "revision": "cecde8efa8f2bad3038e",
+    "url": "./static/js/2.cecde8ef.chunk.js"
+  },
+  {
+    "revision": "f6ceef7a9ec27a961579a2acd2ce57e8",
+    "url": "./index.html"
+  }
+];
