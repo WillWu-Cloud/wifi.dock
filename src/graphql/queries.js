433ffc031e81b5b92eae46d0d8734d5b8e8fbd52
will.wu <will.wu@eitc.com.tw>
2021-01-19 10:52:37 +0800

    initial commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDevice = /* GraphQL */ `
+  query GetDevice($id: ID!) {
+    getDevice(id: $id) {
+      id
+      title
+      description
+      filePath
+      like
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listDevices = /* GraphQL */ `
+  query ListDevices(
+    $filter: ModelDeviceFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDevices(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        description
+        filePath
+        like
+        owner
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateDevice = /* GraphQL */ `
+  subscription OnCreateDevice {
+    onCreateDevice {
+      id
+      title
+      description
+      filePath
+      like
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateDevice = /* GraphQL */ `
+  subscription OnUpdateDevice {
+    onUpdateDevice {
+      id
+      title
+      description
+      filePath
+      like
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteDevice = /* GraphQL */ `
+  subscription OnDeleteDevice {
+    onDeleteDevice {
+      id
+      title
+      description
+      filePath
+      like
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
