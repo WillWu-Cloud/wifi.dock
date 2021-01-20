0a9a521115f2d2ffb94135b1c274e66c2cb8ccde
will.wu <will.wu@eitc.com.tw>
2021-01-20 08:43:08 +0800

    Updates

--- static/js/27.24a6651e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwifi-dock"]=this["webpackJsonpwifi-dock"]||[]).push([[27],{625:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return c}));var i=n(22),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
-//# sourceMappingURL=27.24a6651e.chunk.js.map
