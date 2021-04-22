edaee79fd57b28cb63665552c33bd4e720af6dd2
will.wu <will.wu@eitc.com.tw>
2021-04-22 09:39:11 +0800

    useTable

--- src/components/MemberPage.js
+++ src/components/MemberPage.js
@@ -1,27 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from './PageHeader';
-import { makeStyles } from '@material-ui/core/styles';
+import { 
+  makeStyles, 
+  Paper, 
+  TableBody, 
+  TableRow, 
+  TableCell, 
+  Toolbar, 
+  InputAdornment } from '@material-ui/core';
+import useTable from "./useTable";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1)
     },
-  }
-  
+  },
+  pageContent:{
+    margin: theme.spacing(5),
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(3),
+    paddingTop: theme.spacing(0.5),
+  },
 }));
+
+const headCells = [
+  {id: 'Name', label: 'Member name'},
+  {id: 'email', label: 'Email Address'},
+  {id: 'role', label: 'Role'},
+  {id: 'actions', label: 'Actions', disableSorting: true}
+]
 
 export default function MemberPage() {
 
   const classes = useStyles();
   const filteredMembers = [];
+  const [records, setRecords] = useState([]);
+  const [filterFn, setFilterFn] = useState({fn: items => { return items;}})
+
+  const{
+    TblContainer,
+    TblHead,
+    TblPagination,
+    recordsAfterPaginAndSorting
+  } = useTable(records,headCells,filterFn);
+
   return (
     <div className={classes.root}>
-       <PageHeader 
-          title="Bizlink"
-          subTitle=""
-          qty={`${filteredMembers.length}`}
-          unit="member"/>    
+      <PageHeader 
+        title="Bizlink"
+        subTitle=""
+        qty={`${filteredMembers.length}`}
+        unit="member"/>   
+      <Paper className={classes.pageContent}>
+        <TblContainer>
+          <TblHead />
+          
+        </TblContainer>
+        <TblPagination />     
+      </Paper>      
     </div>
+    
   )
 }
--- src/components/useTable.js
+++ src/components/useTable.js
@@ -23,7 +23,7 @@ export default function useTable(records,headCells,filterFn) {
 
   const classes = useStyles();
 
-  const pages = [3, 4, 5]
+  const pages = [5, 10, 15]
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
   const [order, setOrder] = useState();
